c28c134fa70c93b3a950fd8821b453584e2b48f1
Meyzan16 <Meyzan1605@gmail.com>
2023-05-05 22:47:28 +0700

    perbarui cors client

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.use('/uploads', express.static(__dirname+'/uploads'));
 app.use(cors(
   {
     credentials: true,
-    origin: 'https://client-bookingapp.vercel.app/',
+    origin: 'https://client-bookingapp.vercel.app',
     optionsSuccessStatus: 200,
   }
 ));
